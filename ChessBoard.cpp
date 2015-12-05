e4e1704190c0537da4d2cc4587a2d3c80e1b3008
Izaak Coleman <ic711@vm-shell2.doc.ic.ac.uk>
2015-12-05 19:25:25 +0000

    Skeleton Files for Pieces

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -15,16 +15,20 @@ ChessBoard::ChessBoard
 
 void ChessBoard::loadStartPositions()
 {
-  typedef pair< int, int > squareID;             // Square given rank and file.
-  pair< Board::iterator, bool > validInsert;     // Stores last insertion info.
+  pair< Board::iterator, bool > validInsert;      // Stores last insertion info.
   
   for( int rank=1; rank <= MAX_RANK; rank++ ){
-    for( int file=1; file <= MAX_FILE; file++ ){
-      squareID square ( rank, file );   // Assign rank and file as ID.
-      validInsert = chessboard.insert( pair< square, *ChessPiece );
-//    validInsert = chessboard.insert( allocatePiece( square ) );
+    for( int file=1; file <= MAX_FILE; file++ ){  
+
+      SquareID square ( rank, file );             // Generate a square 
+      validInsert = chessboard.insert( allocatePiece( square ) );
+
+      if( validInsert.second == false ){          // handle insert fail 
+        /* Throw an error */
+      }
     }
   }
+
 }
 
 pair< SquareID, *ChessPiece > ChessBoard::allocatePiece( SquareID square )
@@ -86,7 +90,7 @@ pair< SquareID, *ChessPiece > ChessBoard::allocatePiece( SquareID square )
     cp = new King( BLACK );
     return pair< SquareID, *ChessPiece > ( square, cp );
   }
-  else if( rank == 8 && (file == 5 ) ) {
+  else if( rank == 8 && (file == 5 ) ){
     cp = new Queen( BLACK );
     return pair< SquareID, *ChessPiece > ( square, cp );
   }
@@ -94,37 +98,43 @@ pair< SquareID, *ChessPiece > ChessBoard::allocatePiece( SquareID square )
     cp = new Pawn( BLACK );
     return pair< SquareID, *ChessPiece > ( square, cp );
   }
+  
+  else{ // Given rank and file out of range. Throw exptn.
+    /* Throw exptn */
+  }
 }
 
 
 
-///* internal helper function */
-//void print_frame(int row) {
-//  if (!(row % 3))
-//    cout << "  +===========+===========+===========+" << endl;
-//  else
-//    cout << "  +---+---+---+---+---+---+---+---+---+" << endl;
-//}
-//
-///* internal helper function */
-//void print_row(const char *data, int row) {
-//  cout << (char) ('A' + row) << " ";
-//  for (int i=0; i<9; i++) {
-//    cout << ( (i % 3) ? ':' : '|' ) << " ";
-//    cout << ( (data[i]=='.') ? ' ' : data[i]) << " ";
-//  }
-//  cout << "|" << endl;
-//}
-//
-///* pre-supplied function to display a Sudoku board */
-//void display_board(const char board[9][9]) {
-//  cout << "    ";
-//  for (int r=0; r<9; r++) 
-//    cout << (char) ('1'+r) << "   ";
-//  cout << endl;
-//  for (int r=0; r<9; r++) {
-//    print_frame(r);
-//    print_row(board[r],r);
-//  }
-//  print_frame(9);
-//}
+/* internal helper function */
+void ChessBoard::print_frame() {
+    cout << "  +===========+===========+===========+" << endl;
+}
+
+/* internal helper function */
+void ChessBoard::print_row( const Board &cb, int rank ) {
+  cout << static_cast< char > ( rank ) << " ";
+  for ( int file = 1; file <= MAX_FILE; file++ ) {
+    cout << "|" << " ";
+    if( cb.find( rank, file)->charpiece != NULL ){
+      cout << cb[ make_pair(rank,file) ]->charpiece;
+    }
+    else{
+      cout << " ";
+    }
+  }
+  cout << "|" << endl;
+}
+
+/* pre-supplied function to display a Sudoku board */
+void ChessBoard::display_board( const Board &cb ){
+  cout << "    ";
+  for (int rank = 1; r <= MAX_RANK; rank++) 
+    cout << (char) ('1'+r) << "   ";
+  cout << endl;
+  for (int rank = 1; r < MAX_RANK; rank++) {
+    print_frame();
+    print_row( cb );
+  }
+  print_frame();
+}
--- ChessBoard.h
+++ ChessBoard.h
@@ -6,14 +6,28 @@ private:
   /* Attributes */
   enum moves { BLACK, WHITE };
   moves move;
-  typedef std::map< std::pair<int, int>, ChessPiece* > Board;
+  typedef std::pair< int, int > SquareID;
+
+  typedef std::map< SquareID, ChessPiece* > Board;
   Board chessboard;
 
    
   /* Member Functions */
   ChessBoard();
   /* Calls loadStartPositions, sets move to WHITE */
+
   void loadStartPositions();
+  /* PreCondition:  chessboard member empty.
+     PostCondition: chessboard member contains squares mapped
+     to ChessPieces in the usual chess configuration. */
+
+  std::pair< SquareID, *ChessPiece > allocatePiece( SquareID square );
+  /* Pairs together a square and a ChessPiece. Each square passed in
+     has a rank and file. The Subclass of ChessPiece assigned to the
+     square corresponds to this rank and file. */
+
+  void displayBoard();
+  /* Prints the chessboard to the screen. */
   /* Sets up board with mapped chess pieces */
  // bool submitMove( char* sourceSq, char* destSq );
  // /*void updateBoard( ... ); */
