09220767fc67d93ebbd346cb671cf513d1441894
Izaak Coleman <ic711@vm-shell2.doc.ic.ac.uk>
2015-12-13 21:13:34 +0000

    trying to match model students output

--- main.cpp
+++ main.cpp
@@ -7,32 +7,24 @@ using namespace std;
 int main() {
 
     cout << "===========================" << endl;
-    cout << "Testing the Chess Engine" << endl;
-    cout << "===========================" << endl;
     cout << endl;
-
-    ChessBoard cb;
     cout << endl;
-
-    cb.submitMove("D7", "D6");
     cout << endl;
-
-    cb.submitMove("D4", "H6");
     cout << endl;
-
-    cb.submitMove("D2", "D4");
     cout << endl;
-
-    cb.submitMove("F8", "B4");
     cout << endl;
-
-    cout << "===========================" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
     cout << "Alekhine vs. Vasic (1931)" << endl;
     cout << "===========================" << endl;
     cout << endl;
-
-    cb.resetBoard();
-    cout << endl;
+  
+    ChessBoard cb;
 
     cb.submitMove("E2", "E4");
     cb.submitMove("E7", "E6");
